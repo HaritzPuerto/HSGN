d69426752c8a7f25df2efe9f522e0ac89aeda635
Haritz Puerto <haritzpuerto94@gmail.com>
2020-09-04 13:57:05 +0000

    script ready for inference

--- src/models/document_retrieval.py
+++ src/models/document_retrieval.py
@@ -47,7 +47,7 @@ class DocumentRetrieval():
                         add_special_tokens=True,
                         max_length=128,
                         pad_to_max_length=True,
-                        #truncation=True,
+                        truncation=True,
                         return_attention_mask=True,
                         return_tensors='pt'
                 )
