005ba749277923726b0d6dbb5a5d20c8b7cf49a5
Haritz Puerto <haritzpuerto94@gmail.com>
2020-11-10 11:32:31 +0000

    updated codalab

--- codalab_repo.sh
+++ codalab_repo.sh
@@ -1,1 +1,1 @@
-cl run --request-docker-image naster94/exo --request-network 'apt-get update && apt install git -y; git clone https://github.com/HaritzPuerto/HSGN ; mkdir HSGN/data; mkdir HSGN/data/external'  -n repo 
+cl run --request-docker-image naster94/exo --request-network 'apt-get update && apt install git -y; git clone https://github.com/HaritzPuerto/HSGN ; mkdir HSGN/data; mkdir HSGN/data/external; mkdir HSGN/SAE/models/'  -n repo 
