6f63f5c1f4376bc461e60c17fffdc2bbd45590cb
Haritz Puerto <haritzpuerto94@gmail.com>
2020-12-01 08:42:40 +0000

    no need to download bert large

--- download_nlp_files.py
+++ download_nlp_files.py
@@ -4,7 +4,7 @@ from allennlp.predictors.predictor import Predictor
 
 transformers.BertTokenizer.from_pretrained('bert-base-uncased')
 transformers.BertModel.from_pretrained('bert-base-uncased')
-transformers.BertTokenizer.from_pretrained("bert-large-uncased-whole-word-masking")
-transformers.BertModel.from_pretrained("bert-large-uncased-whole-word-masking")
+#transformers.BertTokenizer.from_pretrained("bert-large-uncased-whole-word-masking")
+#transformers.BertModel.from_pretrained("bert-large-uncased-whole-word-masking")
 stanza.download("en")
 Predictor.from_path("models/srl_model/bert-base-srl-2019.06.17.tar.gz")
