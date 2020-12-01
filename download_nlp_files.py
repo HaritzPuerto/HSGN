3396ce2135c3aea022cbe71fe5ce4229262311a2
Haritz Puerto <haritzpuerto94@gmail.com>
2020-12-01 07:59:55 +0000

    download nlp files added

---
+++ download_nlp_files.py
@@ -0,0 +1,10 @@
+import transformers
+import stanza
+from allennlp.predictors.predictor import Predictor
+
+transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+transformers.BertModel.from_pretrained('bert-base-uncased')
+transformers.BertTokenizer.from_pretrained("bert-large-uncased-whole-word-masking")
+transformers.BertModel.from_pretrained("bert-large-uncased-whole-word-masking")
+stanza.download("en")
+Predictor.from_path("models/srl_model/bert-base-srl-2019.06.17.tar.gz")
--- download_stanza.py
+++
@@ -1,2 +0,0 @@
-import stanza
-stanza.download('en')
